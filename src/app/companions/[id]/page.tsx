ef959ba90283e5af4a9cc296c8240ffb6863a1f3
duyguozpercin <duygucetind@gmail.com>
2025-08-23 23:55:27 +0300

    feat: add Vapi SDK integration and update dependencies

--- src/app/companions/[id]/page.tsx
+++ src/app/companions/[id]/page.tsx
@@ -1,10 +1,22 @@
+import {getCompanion} from "@/lib/actions/companion.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import {redirect} from "next/navigation";
 
+interface CompanionSessionPageProps {
+  params: Promise<{ id: string }>;
+}
+const CompanionSession = async ({params}: CompanionSessionPageProps) => {
+  const {id} = await params;
+  const companion = await getCompanion(id);
+  const user = await currentUser();
 
-const CompanionSession = () => {
+  if (!user) redirect('/sign-in');
+
+  if (!companion) redirect('/companions');
+  
+
   return (
-    <div>
-      CompanionSession
-    </div>
+    <main></main>
   )
 }
 
