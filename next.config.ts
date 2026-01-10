6637c1df7207ff8a25f92ce7fa1d3b66230a03bb
duyguozpercin <duygucetind@gmail.com>
2026-01-10 20:22:14 +0300

    home page

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+      ignoreBuildErrors: true
+    },
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+  images: {
+      remotePatterns: [
+          { hostname: 'img.clerk.com'}
+      ]
+  }
 };
 
 export default withSentryConfig(nextConfig, {
