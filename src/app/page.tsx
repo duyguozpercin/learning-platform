1ce418a2b095299823e0d2627e4c617e3b8012e9
duyguozpercin <duygucetind@gmail.com>
2026-01-20 22:49:14 +0300

    refactor: clean up imports and formatting in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import CompanionCard from '@/components/CompanionCard';
 import CTA from '@/components/CTA';
 import CompanionsList from '@/components/CompanionsList';
-import { recentSessions } from '@/constants';
-import { getAllCompanions, getRecentSessions, getBookmarkedCompanionIds, } from "@/lib/actions/companion.actions";
-import { getSubjectColor } from "@/lib/utils";
-import { auth } from "@clerk/nextjs/server";
+import { getAllCompanions, getRecentSessions, getBookmarkedCompanionIds } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
+import { auth } from '@clerk/nextjs/server';
 
 const Page = async () => {
   const { userId } = await auth();
+
   const companions = await getAllCompanions({ limit: 3 });
   const recentSessionsCompanions = await getRecentSessions(10);
 
@@ -21,8 +21,8 @@ const Page = async () => {
   return (
     <main>
       <h1>Popular Companions</h1>
-      <section className="popular-grid">
 
+      <section className="popular-grid">
         {companions.map((companion) => (
           <CompanionCard
             key={companion.id}
@@ -33,8 +33,8 @@ const Page = async () => {
             revalidatePath="/"
           />
         ))}
-
       </section>
+
       <section className="home-section">
         <CompanionsList
           title="Recently completed sessions"
@@ -43,9 +43,8 @@ const Page = async () => {
         />
         <CTA />
       </section>
-
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
